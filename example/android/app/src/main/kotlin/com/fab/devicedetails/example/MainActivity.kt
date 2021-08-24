dbc277bc75d7dd30399bfc05995e7ed5809a9840
Piero512 <ulloapiero@gmail.com>
2021-08-24 11:17:59 -0500

    Update Android project

--- example/android/app/src/main/kotlin/com/fab/devicedetails/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/fab/devicedetails/example/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.fab.devicedetails.example
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        GeneratedPluginRegistrant.registerWith(this)
-//    }
 }
